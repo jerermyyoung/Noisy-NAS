144bca03fa6d93336f8f2424aefe427aaaa433d7
Hans <jeremyyoung.1125@gmail.com>
2019-09-29 16:59:16 +0800

    first commit

---
+++ utils.py
@@ -0,0 +1,203 @@
+import os
+import numpy as np
+import torch
+import shutil
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+from numpy.testing import assert_array_almost_equal
+
+class AvgrageMeter(object):
+
+  def __init__(self):
+    self.reset()
+
+  def reset(self):
+    self.avg = 0
+    self.sum = 0
+    self.cnt = 0
+
+  def update(self, val, n=1):
+    self.sum += val * n
+    self.cnt += n
+    self.avg = self.sum / self.cnt
+
+
+def accuracy(output, target, topk=(1,)):
+  maxk = max(topk)
+  batch_size = target.size(0)
+
+  _, pred = output.topk(maxk, 1, True, True)
+  pred = pred.t()
+  correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+  res = []
+  for k in topk:
+    correct_k = correct[:k].view(-1).float().sum(0)
+    res.append(correct_k.mul_(100.0/batch_size))
+  return res
+
+
+class Cutout(object):
+    def __init__(self, length):
+        self.length = length
+
+    def __call__(self, img):
+        h, w = img.size(1), img.size(2)
+        mask = np.ones((h, w), np.float32)
+        y = np.random.randint(h)
+        x = np.random.randint(w)
+
+        y1 = np.clip(y - self.length // 2, 0, h)
+        y2 = np.clip(y + self.length // 2, 0, h)
+        x1 = np.clip(x - self.length // 2, 0, w)
+        x2 = np.clip(x + self.length // 2, 0, w)
+
+        mask[y1: y2, x1: x2] = 0.
+        mask = torch.from_numpy(mask)
+        mask = mask.expand_as(img)
+        img *= mask
+        return img
+
+
+def _data_transforms_cifar10(args):
+  CIFAR_MEAN = [0.49139968, 0.48215827, 0.44653124]
+  CIFAR_STD = [0.24703233, 0.24348505, 0.26158768]
+
+  train_transform = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+  ])
+  if args.cutout:
+    train_transform.transforms.append(Cutout(args.cutout_length))
+
+  valid_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+    ])
+  return train_transform, valid_transform
+
+
+def count_parameters_in_MB(model):
+  return np.sum(np.prod(v.size()) for name, v in model.named_parameters() if "auxiliary" not in name)/1e6
+
+
+def save_checkpoint(state, is_best, save):
+  filename = os.path.join(save, 'checkpoint.pth.tar')
+  torch.save(state, filename)
+  if is_best:
+    best_filename = os.path.join(save, 'model_best.pth.tar')
+    shutil.copyfile(filename, best_filename)
+
+
+def save(model, model_path):
+  torch.save(model.state_dict(), model_path)
+
+
+def load(model, model_path):
+  model.load_state_dict(torch.load(model_path))
+
+
+def drop_path(x, drop_prob):
+  if drop_prob > 0.:
+    keep_prob = 1.-drop_prob
+    mask = Variable(torch.cuda.FloatTensor(x.size(0), 1, 1, 1).bernoulli_(keep_prob))
+    x.div_(keep_prob)
+    x.mul_(mask)
+  return x
+
+
+def create_exp_dir(path, scripts_to_save=None):
+  if not os.path.exists(path):
+    os.mkdir(path)
+  print('Experiment dir : {}'.format(path))
+
+  if scripts_to_save is not None:
+    os.mkdir(os.path.join(path, 'scripts'))
+    for script in scripts_to_save:
+      dst_file = os.path.join(path, 'scripts', os.path.basename(script))
+      shutil.copyfile(script, dst_file)
+
+def multiclass_noisify(y, P, random_state=0):
+    """ Flip classes according to transition probability matrix T.
+    It expects a number between 0 and the number of classes - 1.
+    """
+    print(np.max(y), P.shape[0])
+    assert P.shape[0] == P.shape[1]
+    assert np.max(y) < P.shape[0]
+
+    # row stochastic matrix
+    assert_array_almost_equal(P.sum(axis=1), np.ones(P.shape[1]))
+    assert (P >= 0.0).all()
+
+    m = y.shape[0]
+    print(m)
+    new_y = y.copy()
+    flipper = np.random.RandomState(random_state)
+
+    for idx in np.arange(m):
+        i = y[idx]
+        # draw a vector with only an 1
+        flipped = flipper.multinomial(1, P[i, :][0], 1)[0]
+        new_y[idx] = np.where(flipped == 1)[0]
+
+    return new_y
+
+
+# noisify_pairflip call the function "multiclass_noisify"
+def noisify_pairflip(y_train, noise, random_state=None, nb_classes=10):
+    """mistakes:
+        flip in the pair
+    """
+    P = np.eye(nb_classes)
+    n = noise
+
+    if n > 0.0:
+        # 0 -> 1
+        P[0, 0], P[0, 1] = 1. - n, n
+        for i in range(1, nb_classes-1):
+            P[i, i], P[i, i + 1] = 1. - n, n
+        P[nb_classes-1, nb_classes-1], P[nb_classes-1, 0] = 1. - n, n
+
+        y_train_noisy = multiclass_noisify(y_train, P=P,
+                                           random_state=random_state)
+        actual_noise = (y_train_noisy != y_train).mean()
+        assert actual_noise > 0.0
+        print('Actual noise %.2f' % actual_noise)
+        y_train = y_train_noisy
+    print(P)
+
+    return y_train, actual_noise
+
+def noisify_multiclass_symmetric(y_train, noise, random_state=None, nb_classes=10):
+    """mistakes:
+        flip in the symmetric way
+    """
+    P = np.ones((nb_classes, nb_classes))
+    n = noise
+    P = (n / (nb_classes - 1)) * P
+
+    if n > 0.0:
+        # 0 -> 1
+        P[0, 0] = 1. - n
+        for i in range(1, nb_classes-1):
+            P[i, i] = 1. - n
+        P[nb_classes-1, nb_classes-1] = 1. - n
+
+        y_train_noisy = multiclass_noisify(y_train, P=P,
+                                           random_state=random_state)
+        actual_noise = (y_train_noisy != y_train).mean()
+        assert actual_noise > 0.0
+        print('Actual noise %.2f' % actual_noise)
+        y_train = y_train_noisy
+    print(P)
+
+    return y_train, actual_noise
+
+def noisify(nb_classes=10, train_labels=None, noise_type=None, noise_rate=0, random_state=0):
+    if noise_type == 'pairflip':
+        train_noisy_labels, actual_noise_rate = noisify_pairflip(train_labels, noise_rate, random_state=0, nb_classes=nb_classes)
+    if noise_type == 'symmetric':
+        train_noisy_labels, actual_noise_rate = noisify_multiclass_symmetric(train_labels, noise_rate, random_state=0, nb_classes=nb_classes)
+    return train_noisy_labels, actual_noise_rate
